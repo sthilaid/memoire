28d9cb3ebf8179ae47256bebcfcd9487387bac43
David St-Hilaire <sthilaid@iro.umontreal.ca>
2009-10-23 20:17:38 -0400

    fix in benchmark macro

--- redaction/benchmarks/bench_.scm
+++ redaction/benchmarks/bench_.scm
@@ -1,11 +1,10 @@
 (define-macro (time-expr . exprs)
   (let ((time (gensym 'time)))
     `(let ((,time (time->seconds (current-time))))
-       ,@ exprs
+       ,@exprs
        (- (time->seconds (current-time)) ,time))))
 
 (define-macro (define-bench id env . exprs)
-  (define n 500000)
   `(define (,id)
      (let ,env
        (let ((t (time-expr (do ((i 0 (+ i 1)))

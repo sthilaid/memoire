cc7a80b6c7f6d0f280032422e12a5c9736dadb4d
David St-Hilaire <sthilaid@iro.umontreal.ca>
2009-10-29 14:14:51 -0400

    Forgot to commit erl bench update. Also modified the publication date.

--- redaction/benchmarks/erlang_benchmark.erl
+++ redaction/benchmarks/erlang_benchmark.erl
@@ -3,7 +3,7 @@
 -mode(compile).
 
 %-module(erlang_benchmark).
-%-export([pongloop/0, benchrecv/0, benchrecvtimeoutloop/1, main/1]).
+%-export([pongloop/0, send_recv_r/0, bench_send/0, bench_simple_recpt/0, bench_to_recpt/0, benchrecv/0, benchrecvto/0, main/1]).
 
 pingpong(Pid2) ->
 	Pid2 ! {self(), ping},
@@ -23,14 +23,80 @@ pongloop() ->
 			true
 	end.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+send(0) ->  ok;
+send(N) ->  self() ! {allo, 12}, send(N-1).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bench_send() ->
+  {_, T1_s, T1_us} = erlang:now(),
+  send(1000000),
+  {_, T2_s, T2_us} = erlang:now(),
+  receive
+    {result, To} -> To ! T2_s - T1_s + (T2_us - T1_us)/1000000
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bench_simple_recpt_loop(0) -> stop;
+bench_simple_recpt_loop(Index) ->
+  receive
+    {allo, 12} -> benchrecvloop(Index-1)
+  end.
+
+bench_simple_recpt() ->
+  send(1000000),
+  {_, T1_s, T1_us} = erlang:now(),
+  bench_simple_recpt_loop(1000000),
+  {_, T2_s, T2_us} = erlang:now(),
+  receive
+    {result, To} -> To ! T2_s - T1_s + (T2_us - T1_us)/1000000
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+send_recv_r() -> 
+  receive 
+		ping -> 
+           send_recv_r();
+    stop ->
+           ok
+	end.
+
+send_recv_loop_s(R, 0) -> R ! stop;
+send_recv_loop_s(R, I) ->
+  R ! ping,
+  send_recv_loop_s(R, I-1).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bench_to_recpt_loop(0) -> stop;
+bench_to_recpt_loop(Index) ->
+  receive
+    {allo, 12} -> benchrecvloop(Index-1)
+    after 2 -> bench_to_recpt_loop(Index-1)
+  end.
+
+bench_to_recpt() ->
+  send(1000000),
+  {_, T1_s, T1_us} = erlang:now(),
+  bench_to_recpt_loop(1000000),
+  {_, T2_s, T2_us} = erlang:now(),
+  receive
+    {result, To} -> To ! T2_s - T1_s + (T2_us - T1_us)/1000000
+  end.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 benchrecvloop(0) -> stop;
 benchrecvloop(Index) ->
   receive
-    allo -> benchrecvloop(Index-1)
+    salut -> benchrecvloop(Index-1) ;
+    {allo, 12} -> benchrecvloop(Index-1)
   end.
-
-send(0) ->  ok;
-send(N) ->  self() ! allo, send(N-1).
 
 benchrecv() ->
   send(1000000),
@@ -41,22 +107,72 @@ benchrecv() ->
     {result, To} -> To ! T2_s - T1_s + (T2_us - T1_us)/1000000
   end.
 
-benchrecvtimeoutloop(0) -> stop;
-benchrecvtimeoutloop(Index) ->
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+benchrecvtoloop(0) -> stop;
+benchrecvtoloop(Index) ->
   receive
-    after 0 -> benchrecvtimeoutloop(Index-1)
+    salut -> benchrecvtoloop(Index-1) ;
+    {allo, 12} -> benchrecvtoloop(Index-1)
+    after 2 -> benchrecvtoloop(Index-1)
   end.
 
+benchrecvto() ->
+  send(1000000),
+  {_, T1_s, T1_us} = erlang:now(),
+  benchrecvtoloop(1000000),
+  {_, T2_s, T2_us} = erlang:now(),
+  receive
+    {result, To} -> To ! T2_s - T1_s + (T2_us - T1_us)/1000000
+  end.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 main(_) ->
+
+  %% Message sending benchmark %%
+  SendBenchPid = spawn(erlang_benchmark, bench_send, []),
+  SendBenchPid ! {result, self()},
+  receive
+    SendBenchValue -> io:format("(erlang !: ~w)~n",[SendBenchValue])
+  end,
+
+  %% Simple reception benchmark %%
+  SimpleRecptBenchPid = spawn(erlang_benchmark, bench_simple_recpt, []),
+  SimpleRecptBenchPid ! {result, self()},
+  receive
+    SimpleRecptBenchValue -> io:format("(erlang ?: ~w)~n",[SimpleRecptBenchValue])
+  end,
+
+  %% Simple reception with timeout benchmark %%
+  ToRecptBenchPid = spawn(erlang_benchmark, bench_to_recpt, []),
+  ToRecptBenchPid ! {result, self()},
+  receive
+    ToRecptBenchValue -> io:format("(erlang ?-timeout: ~w)~n",[ToRecptBenchValue])
+  end,
+
+  %% Send / Receive benchmark %%
+	Pid2_SR = spawn(erlang_benchmark, send_recv_r, []),
+  {_, T1_s_sr, T1_us_sr} = erlang:now(),
+  send_recv_loop_s(Pid2_SR, 1000000),
+  {_, T2_s_sr, T2_us_sr} = erlang:now(),
+  io:format("(erlang !+?: ~w)~n",[T2_s_sr - T1_s_sr + (T2_us_sr - T1_us_sr)/1000000]),
+
+  %% Recv benchmark %%
   RecvBenchPid = spawn(erlang_benchmark, benchrecv, []),
   RecvBenchPid ! {result, self()},
   receive
     RecvBenchValue -> io:format("(erlang recv: ~w)~n",[RecvBenchValue])
   end,
-  {_, T1_s_a, T1_us_a} = erlang:now(),
-  benchrecvtimeoutloop(1000000),
-  {_, T2_s_a, T2_us_a} = erlang:now(),
-  io:format("(erlang recv-timeout: ~w)~n",[T2_s_a - T1_s_a + (T2_us_a - T1_us_a)/1000000]),
+
+  %% Recv with timeout benchmark %%
+  RecvToBenchPid = spawn(erlang_benchmark, benchrecvto, []),
+  RecvToBenchPid ! {result, self()},
+  receive
+    RecvToBenchValue -> io:format("(erlang recv-timeout: ~w)~n",[RecvToBenchValue])
+  end,
+
+  %% Ping server benchmark %%
 	Pid2 = spawn(erlang_benchmark, pongloop, []),
   {_, T1_s, T1_us} = erlang:now(),
   pingloop(Pid2, 1000000),
